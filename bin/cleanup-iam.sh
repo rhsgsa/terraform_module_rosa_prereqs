a64fd686c1b8f89ea0705c15755aec801c135321
Tsai Li Ming <tsai.liming@gmail.com>
2021-09-19 00:52:13 +0800

    Add dry run option

--- bin/cleanup-iam.sh
+++ bin/cleanup-iam.sh
@@ -5,33 +5,55 @@ if [ -z "${ROLE_PREFIX+x}" ]; then
   exit -1
 fi
 
+opt_dry_run=false
+while :; do
+    case $1 in
+        --dry-run) 
+          opt_dry_run=true
+          echo "***In dry run mode***"
+        ;;
+        *) break
+    esac
+    shift
+done
+
+if "$opt_dry_run"; then
+    cmd="echo > DRYRUN: $*"
+else
+    cmd=''
+fi
+
+echo "Using ROLE_PREFIX: $ROLE_PREFIX"
+
 for name in `aws iam list-roles | jq -r .Roles[].RoleName | grep ^$ROLE_PREFIX`; do 
   
-  echo "Processing role: $name"
+  echo "Processing IAM role: $name"
 
-  for profile in ` aws iam list-instance-profiles-for-role --role-name $name | jq -r .InstanceProfiles[].InstanceProfileName`; do
-    aws iam remove-role-from-instance-profile --instance-profile-name $profile --role-name $name
-    
-    echo "Deleting instance profile: $profile"
-    aws iam delete-instance-profile --instance-profile-name $profile
+  for profile in `aws iam list-instance-profiles-for-role --role-name $name | jq -r .InstanceProfiles[].InstanceProfileName`; do
+    echo "Remove IAM role: $name from instance profile: $profile"
+    $cmd aws iam remove-role-from-instance-profile --instance-profile-name $profile --role-name $name
+    echo "Deleting IAM instance profile: $profile"
+    $cmd aws iam delete-instance-profile --instance-profile-name $profile
   done
 
   for policy in `aws iam list-attached-role-policies --role-name $name | jq -r .AttachedPolicies[].PolicyArn`; do
-    aws iam detach-role-policy --role-name $name --policy-arn $policy
-    echo "Deleting policy: $policy"
-    aws iam delete-policy --policy-arn $policy
+    echo "Detach IAM policy: $policy from role: $name"
+    $cmd aws iam detach-role-policy --role-name $name --policy-arn $policy
+    echo "Deleting IAM policy: $policy"
+    $cmd aws iam delete-policy --policy-arn $policy
   done 
 
   for policy in `aws iam list-role-policies --role-name $name | jq -r .PolicyNames[]`; do 
-    echo "Deleting role policy: $policy"
-    aws iam delete-role-policy --role-name $name --policy-name $policy
+    echo "Deleting IAM role policy: $policy"
+    $cmd aws iam delete-role-policy --role-name $name --policy-name $policy
   done	  
 
-  echo "Deleting role: $name"
-  aws iam delete-role --role-name $name
+  echo "Deleting IAM role: $name"
+  $cmd aws iam delete-role --role-name $name
+  echo 
 done
 
 for policy in `aws iam list-policies  | jq -r .Policies[].Arn | egrep "^arn:aws:iam::[0-9]+:policy/$ROLE_PREFIX"`; do 
-  echo "Deleting policy: $policy"
-  aws iam delete-policy --policy-arn $policy
+  echo "Deleting IAM policy: $policy"
+  $cmd aws iam delete-policy --policy-arn $policy
 done
